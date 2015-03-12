e6d969421118c3c496e1864220295655a2e9e5d9
Narbe Voskanian <narbevoskanian@gmail.com>
2015-03-11 18:12:20 -0700

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@
 REQUEST_STATUSES = %w(PENDING)
 
 15.times do
-  CatRentalRequest.create!(cat_id: rand(1..2), start_date: rand(50..100).days.ago, end_date: rand(1..49).days.ago, status: REQUEST_STATUSES[0], user_id: rand(3..5))
+  CatRentalRequest.create!(cat_id: rand(1..15), start_date: rand(50..100).days.ago, end_date: rand(1..49).days.ago, status: REQUEST_STATUSES[0], user_id: rand(1..2))
 end
 
 15.times do
-  CatRentalRequest.create!(cat_id: rand(1..2), start_date: rand(1..49).days.ago, end_date: rand(1..25).days.from_now, status: REQUEST_STATUSES[0], user_id: rand(3..5))
+  CatRentalRequest.create!(cat_id: rand(1..15), start_date: rand(1..49).days.ago, end_date: rand(1..25).days.from_now, status: REQUEST_STATUSES[0], user_id: rand(1..2))
 end
 
 
@@ -20,8 +20,7 @@ User.create(user_name: 'neff', password: 'neffneff')
 User.create(user_name: 'cj', password: 'cjisthebest')
 User.create(user_name: 'daniel', password: 'dantheman')
 ## cats ##
-
-Cat.create(name: Faker::Name.name, color: 'gold', sex: 'F', user_id: 1, birth_date: Faker::Date.backward(1000))
-Cat.create(name: Faker::Name.name, color: 'gold', sex: 'F', user_id: 2, birth_date: Faker::Date.backward(1000))
-
+15.times do
+  Cat.create(name: Faker::Name.first_name, color: 'gold', sex: 'F', user_id: rand(1..3), birth_date: Faker::Date.backward(1000))
+  end
 ###
